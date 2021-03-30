9d7e4251732f41680b02746b26de815239d344f3
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-30 23:31:20 +0900

    Navbar 컴포넌트 작성

---
+++ front/src/components/Navbar/index.tsx
@@ -0,0 +1,53 @@
+import React, { useCallback, useState } from "react";
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavBtn,
+  NavBtnLink,
+  NavItem,
+  NavLinks,
+  NavLogo,
+  NavMenu,
+} from "./styles";
+import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+
+interface INavbarProps {
+  toggleMenu: () => void;
+}
+function Navbar({ toggleMenu }: INavbarProps) {
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">GGyu</NavLogo>
+            <MobileIcon onClick={toggleMenu}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to="about">About</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="discover">Discover</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="services">Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="signup">Sign Up</NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/signin">Sign In</NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
