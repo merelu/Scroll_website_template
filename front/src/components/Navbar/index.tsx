ecba8f12fa60a7705695454ec27ee793e379d501
GYUHA_HWANG <gyuha0310@gmail.com>
2021-04-05 23:33:50 +0900

    navbar 스크롤 효과 추가

--- front/src/components/Navbar/index.tsx
+++ front/src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   MobileIcon,
   Nav,
@@ -15,29 +15,82 @@ import { IconContext } from "react-icons/lib";
 
 interface INavbarProps {
   toggleMenu: () => void;
+  toggleHome: () => void;
 }
-function Navbar({ toggleMenu }: INavbarProps) {
+function Navbar({ toggleMenu, toggleHome }: INavbarProps) {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = useCallback(() => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+
+    return () => {
+      window.removeEventListener("scroll", changeNav);
+    };
+  }, [changeNav]);
+
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
-        <Nav>
+        <Nav scrollNav={scrollNav}>
           <NavbarContainer>
-            <NavLogo to="/">GGyu</NavLogo>
+            <NavLogo to="/" onClick={toggleHome}>
+              GGyu
+            </NavLogo>
             <MobileIcon onClick={toggleMenu}>
               <FaBars />
             </MobileIcon>
             <NavMenu>
               <NavItem>
-                <NavLinks to="about">About</NavLinks>
+                <NavLinks
+                  to="about"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  offset={-80}
+                >
+                  About
+                </NavLinks>
               </NavItem>
               <NavItem>
-                <NavLinks to="discover">Discover</NavLinks>
+                <NavLinks
+                  to="discover"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  offset={-80}
+                >
+                  Discover
+                </NavLinks>
               </NavItem>
               <NavItem>
-                <NavLinks to="services">Services</NavLinks>
+                <NavLinks
+                  to="services"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  offset={-80}
+                >
+                  Services
+                </NavLinks>
               </NavItem>
               <NavItem>
-                <NavLinks to="signup">Sign Up</NavLinks>
+                <NavLinks
+                  to="signup"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  offset={-80}
+                >
+                  Sign Up
+                </NavLinks>
               </NavItem>
             </NavMenu>
             <NavBtn>
