941d74407f70cd7f81625dc2b1b56b4a9b990327
GYUHA_HWANG <gyuha0310@gmail.com>
2021-04-05 23:31:36 +0900

    route setting

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,18 @@
 import Home from "@pages/Home";
+import Signin from "@pages/Signin";
 import React from "react";
+import { Route, Switch } from "react-router";
 import "./App.css";
 
 function App() {
-  return <Home></Home>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/signin" component={Signin} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
